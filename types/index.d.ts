1d65d8e9ce459c3287ca6aa3c59cdb1062b388a1
marcantondahmen <marcantondahmen@users.noreply.github.com>
2023-01-23 13:46:24 +0000

    deploy: 212c8c1161bd5f19f1c89e1aa0140aa5f7974d23

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,3 @@
-/*!
- * Sortable Tree
- *
- * (c) 2023 Marc Anton Dahmen, MIT license
- */
-import 'modern-normalize/modern-normalize.css';
-import './styles/styles.less';
-export * from './SortableTree';
-export * from './SortableTreeNode';
-export * from './types';
+import './src/styles.less';
+export * from './src';
+export * from './demo';
---
+++ types/src/index.d.ts
@@ -0,0 +1,8 @@
+/*!
+ * Sortable Tree
+ *
+ * (c) 2023 Marc Anton Dahmen, MIT license
+ */
+export * from './SortableTree';
+export * from './SortableTreeNode';
+export * from './types';
